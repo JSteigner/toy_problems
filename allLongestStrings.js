3962a068e433a386b3f51f957f011f0f7bd68bef
Jay Steigner <ioctosteigner@gmail.com>
2021-02-16 19:38:13 -0600

    (add) added alternate solution to allLongestStrings

--- allLongestStrings.js
+++ allLongestStrings.js
@@ -1,18 +1,27 @@
-const allLongestStrings = (inputArray) => {
-  const longestStringLength = longestStringLengthFinder(inputArray);
-  const longestStrings = [];
-  inputArray.forEach(string => {
-    if (string.length === longestStringLength) longestStrings.push(string);    
-  });
-  return longestStrings;
+const allLongestStringsAlternate = (inputArray) => {
+  const maxSize = Math.max(...inputArray.map(string => string.length));
+  return inputArray.filter(string => string.length === maxSize);
 }
+console.log(allLongestStringsAlternate(["aba", "aa", "ad", "vcd", "aba"])); //  ["aba", "vcd", "aba"]. 
 
-const longestStringLengthFinder = (inputArray) => {
-  let longestStringLength = 0;
-  inputArray.forEach(string => {
-    if (string.length > longestStringLength) longestStringLength = string.length;    
-  });
-  return longestStringLength;
-}
+// const allLongestStrings = (inputArray) => {
+//   const longestStringLength = longestStringLengthFinder(inputArray);
+//   const longestStrings = [];
+//   inputArray.forEach(string => {
+//     if (string.length === longestStringLength) longestStrings.push(string);    
+//   });
+//   return longestStrings;
+// }
+
+// const longestStringLengthFinder = (inputArray) => {
+//   let longestStringLength = 0;
+//   inputArray.forEach(string => {
+//     if (string.length > longestStringLength) longestStringLength = string.length;    
+//   });
+//   return longestStringLength;
+// }
 
 // console.log(allLongestStrings(["aba", "aa", "ad", "vcd", "aba"])); //  ["aba", "vcd", "aba"]. 
+
+
+
